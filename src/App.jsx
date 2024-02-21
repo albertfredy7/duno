e25b542b06ffc02b6be3c39086a75a8a8769714e
abie_x <abhiramzmenon@gmail.com>
2024-02-21 11:49:22 +0530

    checking merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 // is it works??
-// wow it works
+// wow its not working
 
 function App() {
   
